b03f2c2b220e3543dfb9928493714223255d9b31
Karinate <41171307+Karinate@users.noreply.github.com>
2021-12-14 08:28:07 +0100

    Update volFieldI.h

--- src/volFieldI.h
+++ src/volFieldI.h
@@ -428,33 +428,35 @@ for (unsigned int i=0; i < v1.size(); i++) {
 
 // G5 - Contribution
 // To compute a scalarField to store the magnitude of the vectorField
-/* template <typename vectorType>
+template <typename vectorType>
 scalarField volField<vectorType>::magVector()
 {
   vector3 v1;
   volField<scalarField> result;
-if (typeid(internalField).hash_code()==typeid(v1).hash_code() {
-    string magFieldFname="magof"+fieldName_;
-
-       result.fieldName_=magFieldFname;
-       result.runTime_=runTime_;
-
-for(unsigned int i= 0; i < internalField.size(); i++) {
+  if (typeid(internalField).hash_code()==typeid(v1).hash_code() {
+     string magFieldFname ="magof"+fieldName_;
+ 
+     result.fieldName_=magFieldFname;
+     result.runTime_=runTime_;
+ 
+    for(unsigned int i= 0; i < internalField.size(); i++) {
     result.internalField.push_back(mag(internalField[i]));
- }
-
-for(unsigned int i = 0; i < mesh_.nPatches_; i++){
- for(unsigned int i = 0; i < mesh_.nPatches_; i++){
+  }
+ 
+  for(unsigned int i = 0; i < mesh_.nPatches_; i++){
+ 
+    for(unsigned int i = 0; i < mesh_.nPatches_; i++){
        result.boundaryField_[i].type = "fixedValue";
        result.boundaryField_[i].valImposed = true;
-      for(unsigned int j = 0; j < result.boundaryField_[i].fieldValue.size(); j++){
-         result.boundaryField_[i].fieldValue.push_back (mag(boundaryField_[i].fieldValue[j])); 
+       for(unsigned int j = 0; j < result.boundaryField_[i].fieldValue.size(); j++){
+          result.boundaryField_[i].fieldValue.push_back (mag(boundaryField_[i].fieldValue[j]));  
       }
     }
-   }
   }
+}
 else
- {
-cout:: << "Error: Function volField<vectorType>::magVector called for a non vector Field";
-  }
-return result; */
+{
+ cout:: << "Error: Function volField<vectorType>::magVector called for a non vector Field";
+}
+return result;
+}
